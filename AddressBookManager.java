ca9767f59d480a3feef082b3ed84530e17c9db5c
aytech11 <aytech0110@gmail.com>
2022-10-14 21:11:12 +0530

    no duplicate entry

---
+++ AddressBookManager.java
@@ -0,0 +1,11 @@
+package bl.com.addressbook;
+
+public class AddressBookManager {
+
+	public static void main(String[] args) {
+		AddressBook entry = new AddressBook();
+	
+		System.out.println("Welcome to Address Book Program in Java!");
+		entry.manageAddressBookList();
+	}
+}
